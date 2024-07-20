951f7467bdec7fb0d8b27eeed89685f3776036ac
Tara <hymerstara@gmail.com>
2024-07-20 16:35:05 -0600

    "Create: 3 stores in the db"

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,11 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+# Creating stores in the database
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+# Outputting the number of stores to ensure the correct count
+puts "Number of stores: #{Store.count}"
